ba6c5cbebcb380dd65eae06e5a9002f9dd781cb4
maskedmarker <changjianghui0822@gmail.com>
2025-07-06 19:31:46 +0800

    add

--- ch01-hello/src/test/java/org/example/projectreactor/hello/HttpClientTest.java
+++ ch01-hello/src/test/java/org/example/projectreactor/hello/HttpClientTest.java
@@ -1,6 +1,8 @@
 package org.example.projectreactor.hello;
 
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.util.CharsetUtil;
 import org.junit.Test;
@@ -10,6 +12,8 @@ import reactor.netty.ByteBufFlux;
 import reactor.netty.Connection;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
+
+import java.net.InetSocketAddress;
 
 public class HttpClientTest {
 
@@ -81,7 +85,7 @@ public class HttpClientTest {
     }
 
     @Test
-    public void test20() {
+    public void test6() {
         HttpClient client =
                 HttpClient.create()
                         .headers(h -> h.set(HttpHeaderNames.CONTENT_LENGTH, 5));
@@ -140,5 +144,63 @@ public class HttpClientTest {
                     return bytes.asString();
                 })
                 .block();
+    }
+
+    @Test
+    public void test20() {
+        HttpClient client =
+                HttpClient.create()
+                        .bindAddress(() -> new InetSocketAddress("host", 1234))
+                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                        .option(ChannelOption.SO_KEEPALIVE, true)
+                        // The options below are available only when NIO transport (Java 11) is used
+                        // on Mac or Linux (Java does not currently support these extended options on Windows)
+                        // https://bugs.openjdk.java.net/browse/JDK-8194298
+                        //.option(NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPIDLE), 300)
+                        //.option(NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPINTERVAL), 60)
+                        //.option(NioChannelOption.of(ExtendedSocketOptions.TCP_KEEPCOUNT), 8);
+                        // The options below are available only when Epoll transport is used
+                        .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+                        .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+                        .option(EpollChannelOption.TCP_KEEPCNT, 8);
+
+        String response =
+                client.get()
+                        .uri("https://example.com/")
+                        .responseContent()
+                        .aggregate()
+                        .asString()
+                        .block();
+
+        System.out.println("Response " + response);
+    }
+
+    @Test
+    public void test21() {
+        HttpClient client =
+                HttpClient.newConnection()
+                        .doOnConnected(conn -> System.out.println("Connection " + conn.channel()));
+
+        String response =
+                // A new connection is established for every request
+                client.get()
+                        .uri("https://httpbin.org/get")
+                        .responseContent()
+                        .aggregate()
+                        .asString()
+                        .block();
+
+        System.out.println("Response " + response);
+
+        response =
+                // A new connection is established for every request
+                client.post()
+                        .uri("https://httpbin.org/post")
+                        .responseContent()
+                        .aggregate()
+                        .asString()
+                        .block();
+
+        System.out.println("Response " + response);
     }
 }
